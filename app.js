4e2b7b5988c842f126f8393cb8fa17672e77eb0b
Andrew <yu.drew@gmail.com>
2014-12-10 13:44:29 -0800

    created views

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
-var setupOdysseyRoutes = require('./lib/odyssey/routes');
+
+// Tell application where the views are 
+var path = require('path') // path is included in node, just need to require it to use it
+app.set('views', path.join(__dirname, 'lib', 'views'));
+app.set('view engine', 'jade');
 
 // Pass 'app' to routes 
+var setupOdysseyRoutes = require('./lib/odyssey/routes');
 setupOdysseyRoutes(app);
 
 // Start the app, and begin listening on port 3000
